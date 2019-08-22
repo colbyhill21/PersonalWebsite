e7e09dd0842304e00d7874a27d7d1e28648d0b9c
Colby Hill <colbyhill21@yahoo.com>
2019-08-22 16:04:26 -0400

    Working on the about section now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/Home/home.component'
+import { AboutComponent } from './components/About/about.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {  path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
