320d095f6e3d2f971f7788bb2739118fbadfe52c
Colby Hill <colbyhill21@yahoo.com>
2019-08-25 14:21:44 -0400

    Optimized the experience component UI for mobile

--- src/app/components/Experience/experience.component.ts
+++ src/app/components/Experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
     selector: 'app-experience',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
     styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent {
-
+    onDesktop = true;
+    innerWidth: any;
+    ngOnInit() {
+        this.innerWidth = window.innerWidth;
+        if (this.innerWidth < 768) {
+            this.onDesktop = false;
+        }
+        else {
+            this.onDesktop = true;
+        }
+    }
+    @HostListener('window:resize', ['$event']) //dynamically update the width on resize.
+    onResize(event: { target: { innerWidth: any; }; }) {
+        this.innerWidth = event.target.innerWidth;
+        if (this.innerWidth < 768) {
+            this.onDesktop = false;
+        }
+        else {
+            this.onDesktop = true;
+        }
+    }
 }
