969bfc984d59c9d6d9892573ab372fd025250922
colbyhill21 <acehill21@ymail.com>
2020-01-31 10:46:47 -0500

    Fixed mobile layout for experience section

--- src/app/components/Home/experience-section/experience-section.component.ts
+++ src/app/components/Home/experience-section/experience-section.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-experience-section',
-  templateUrl: './experience-section.component.html',
-  styleUrls: ['./experience-section.component.css']
+    selector: 'app-experience-section',
+    templateUrl: './experience-section.component.html',
+    styleUrls: ['./experience-section.component.css']
 })
-export class ExperienceSectionComponent implements OnInit {
+export class ExperienceSectionComponent {
+    onDesktop = true;
+    innerWidth: any;
+    ngOnInit() {
+        this.innerWidth = window.innerWidth;
+        this.adjustHeight();
+    }
+    @HostListener('window:resize', ['$event']) //dynamically update the width on resize.
+    onResize(event: { target: { innerWidth: any; }; }) {
+        this.innerWidth = event.target.innerWidth;
+        this.adjustHeight();
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    adjustHeight() {
+        if (this.innerWidth < 768) {
+            this.onDesktop = false;
+        }
+        else {
+            this.onDesktop = true;
+        }
+    }
 
 }
