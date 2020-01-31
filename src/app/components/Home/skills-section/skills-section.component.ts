8d540eaf59fe373ba1848ebca9bffef90f97c8fa
colbyhill21 <acehill21@ymail.com>
2020-01-31 11:13:36 -0500

    Optimized experience section for mobile

--- src/app/components/Home/skills-section/skills-section.component.ts
+++ src/app/components/Home/skills-section/skills-section.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-skills-section',
-  templateUrl: './skills-section.component.html',
-  styleUrls: ['./skills-section.component.css']
+    selector: 'app-skills-section',
+    templateUrl: './skills-section.component.html',
+    styleUrls: ['./skills-section.component.css']
 })
-export class SkillsSectionComponent implements OnInit {
+export class SkillsSectionComponent {
+    onDesktop = true;
+    innerWidth: any;
+    ngOnInit() {
+        this.innerWidth = window.innerWidth;
+        this.adjustHeight();
+    }
+    @HostListener('window:resize', ['$event']) //dynamically update the width on resize.
+    onResize(event: { target: { innerWidth: any; }; }) {
+        this.innerWidth = event.target.innerWidth;
+        this.adjustHeight();
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    adjustHeight() {
+        if (this.innerWidth < 768) {
+            this.onDesktop = false;
+        }
+        else {
+            this.onDesktop = true;
+        }
+    }
 }
