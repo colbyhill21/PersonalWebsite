81cd7cd553b87e79922ee617afceeddb3c329fe5
colbyhill21 <acehill21@ymail.com>
2020-07-24 13:46:44 -0600

    Removed skills component

--- src/app/components/Home/skills-section/skills-section.component.ts
+++ src/app/components/Home/skills-section/skills-section.component.ts
@@ -1,28 +1,28 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+// import { Component, HostListener, OnInit } from '@angular/core';
 
-@Component({
-    selector: 'app-skills-section',
-    templateUrl: './skills-section.component.html',
-    styleUrls: ['./skills-section.component.css']
-})
-export class SkillsSectionComponent implements OnInit {
-    onDesktop = true;
-    innerWidth: any;
-    ngOnInit() {
-        this.innerWidth = window.innerWidth;
-        this.adjustHeight();
-    }
-    @HostListener('window:resize', ['$event']) // dynamically update the width on resize.
-    onResize(event: { target: { innerWidth: any; }; }) {
-        this.innerWidth = event.target.innerWidth;
-        this.adjustHeight();
-    }
+// @Component({
+//     selector: 'app-skills-section',
+//     templateUrl: './skills-section.component.html',
+//     styleUrls: ['./skills-section.component.css']
+// })
+// export class SkillsSectionComponent implements OnInit {
+//     onDesktop = true;
+//     innerWidth: any;
+//     ngOnInit() {
+//         this.innerWidth = window.innerWidth;
+//         this.adjustHeight();
+//     }
+//     @HostListener('window:resize', ['$event']) // dynamically update the width on resize.
+//     onResize(event: { target: { innerWidth: any; }; }) {
+//         this.innerWidth = event.target.innerWidth;
+//         this.adjustHeight();
+//     }
 
-    adjustHeight() {
-        if (this.innerWidth < 768) {
-            this.onDesktop = false;
-        } else {
-            this.onDesktop = true;
-        }
-    }
-}
+//     adjustHeight() {
+//         if (this.innerWidth < 768) {
+//             this.onDesktop = false;
+//         } else {
+//             this.onDesktop = true;
+//         }
+//     }
+// }
